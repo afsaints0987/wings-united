7c0f378210f4192dc324d1733192cd3229e8ea4a
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-05-19 17:34:48 +0800

    remove link

--- src/components/Headline/Headline.jsx
+++ src/components/Headline/Headline.jsx
@@ -1,6 +1,6 @@
 import './headline.scss'
 import * as FaIcons from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 const Headline = () => {
   return (
@@ -12,14 +12,14 @@ const Headline = () => {
             
         </h1>
         <h2>Everyday!</h2>
-        <Link to="/home"><p>Proceed to Website &rarr;</p></Link>
+        {/* <Link to="/home"><p>Proceed to Website &rarr;</p></Link> */}
 
         <div id="socials">
             <p className="text-light mx-2">Follow Us:</p>
             <span className="icons">
-                <FaIcons.FaInstagram/>
-                <FaIcons.FaFacebookSquare className="mx-4"/>
-                <FaIcons.FaTiktok/>
+                <a href="https://www.instagram.com/wingsunited"><FaIcons.FaInstagram/></a>
+                <a href="https://www.facebook.com/wingsunited22"><FaIcons.FaFacebookSquare className="mx-4"/></a>
+                <a href="https://www.tiktok.com/@wingsunited22"><FaIcons.FaTiktok/></a>
             </span>
         </div>
     </div>
